257b2c696952062fb898e745edb5b47239f50e83
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-06-15 21:27:19 +0800

    Add some tests

--- tests/grammar_test.cc
+++ tests/grammar_test.cc
@@ -30,4 +30,31 @@ TEST(Grammar, ExpressionTest) {
   const char* EXP1 = "dialogue test { $temp = 12 + 3 * 4 }";
   Parser parser1{EXP1, std::strlen(EXP1)};
   auto ast1 = parser1.GenerateAST();
+
+  EXPECT_EQ(ast1->i18n, nullptr);
+  EXPECT_EQ(ast1->dialogues->dialogue->type.type, TokenType::TOKEN_EMPTY);
+  EXPECT_EQ(ast1->dialogues->dialogue->condition, nullptr);
+  EXPECT_EQ(ast1->dialogues->dialogue->name.value, 0);
+
+  auto cmd1 = ast1->dialogues->dialogue->content->command;
+  auto assign1 = std::dynamic_pointer_cast<Assign>(cmd1);
+  EXPECT_NE(assign1, nullptr);
+  EXPECT_EQ(assign1->target.value, 1);
+  EXPECT_EQ(assign1->expression->op, OperatorType::ADD);
+  EXPECT_EQ(assign1->expression->left->value.value, 12);
+  EXPECT_EQ(assign1->expression->right->op, OperatorType::MUL);
+  EXPECT_EQ(assign1->expression->right->left->value.value, 3);
+  EXPECT_EQ(assign1->expression->right->right->value.value, 4);
+
+  const char* EXP2 = "dialogue test { $temp = 12 + 3 * 4 == 24 }";
+  Parser parser2{EXP2, std::strlen(EXP2)};
+  auto ast2 = parser2.GenerateAST();
+  auto cmd2 = ast2->dialogues->dialogue->content->command;
+  auto assign2 = std::dynamic_pointer_cast<Assign>(cmd2);
+  EXPECT_EQ(assign2->target.value, 1);
+  EXPECT_EQ(assign2->expression->op, OperatorType::EQUAL);
+  EXPECT_EQ(assign2->expression->right->value.value, 24);
+  EXPECT_EQ(assign2->expression->left->op, OperatorType::ADD);
+
+  
 }
